afaafcd607883b97df13cb8ec271af380ca1492a
Kevin Tan <kvintan26@gmail.com>
2024-12-24 09:34:57 +0700

    Update Navbar

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -3,7 +3,7 @@
 <a {{ $attributes }}
     class="{{ $active
         ? 'bg-DDE4DC-bg text-1C4816-text rounded-radius-77 font-weight-750 scale-110 shadow-md w-auto font-arimo'
-        : (Request::is('store', 'register', 'login', 'profile', 'cart')
+        : (Request::is('store', 'register', 'login', 'profile', 'cart', 'menu')
             ? 'text-black hover:underline'
             : 'text-white hover:underline font-arimo font-normal text-base') }} rounded-md px-[0.8vw] py-[0.4vw] text-sm font-medium"
     aria-current="{{ $active ? 'page' : false }}">{{ $slot }}</a>
