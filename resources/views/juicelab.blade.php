e1bad2e6e038595e0276dd8168dd38c56657c272
Kevin Tan <kvintan26@gmail.com>
2024-12-25 15:39:43 +0700

    Menambahkan User Interface JuiceLab

--- resources/views/juicelab.blade.php
+++ resources/views/juicelab.blade.php
@@ -1,2 +1,83 @@
 <x-layout>
+    <div class="relative h-[60vh]">
+        <img class="w-full mt-[-13vh]" src="{{ asset('images/juicelab-bg.svg') }}" alt="JuiceLab Background">
+        <img class="relative mt-[-50vh] ml-[24vw]" src="{{ asset('images/juicelab-hiasan-jeruk.svg') }}"
+            alt="JuiceLab Hiasan Jeruk">
+        <img class="relative mt-[-95vh] ml-[56vw]" src="{{ asset('images/juicelab-hiasan-botol.svg') }}"
+            alt="JuiceLab Hiasan Botol">
+        <img class="relative mt-[-60vh] ml-[9vw]" src="{{ asset('images/juicelab-title.svg') }}" alt="JuiceLab Title">
+        <img class="relative ml-[9vw]" src="{{ asset('images/juicelab-description.svg') }}" alt="JuiceLab Description">
+        <img class="relative ml-[9vw] mt-[3vh]" src="{{ asset('images/juicelab-try-it.svg') }}" alt="JuiceLab Try It">
+    </div>
+
+
+    <div class="bg-[#FDF6E1] relative w-full h-[80vh] z-[-10] mt-[20vh] pt-[15vh]">
+        <div class="relative w-[30vw] h-[10vh] ml-[38vw] flex flex-row">
+            <p class="font-afacad text-[5vw] font-bold text-[#000000]">How It </p>
+            <p class="font-afacad text-[5vw] font-bold text-[#FD7034] ml-[0.5vw]">Works</p>
+        </div>
+
+        <div class="relative w-[80vw] h-[40vh] flex flex-row gap-[5vw] mt-[5vh] ml-[10vw]">
+            <img src="{{ asset('images/juicelab-howitworks-3-tips.svg') }}" alt="JuiceLab How It Works 3 Steps">
+        </div>
+
+    </div>
+
+
+    <div class="relative h-[50vh]">
+        <img class="relative w-full" src="{{ asset('images/juicelab-60k.svg') }}" alt="JuiceLab 60k">
+    </div>
+
+
+    <div class="relative w-full h-[160vh]">
+        <div class="relative w-[30vw] h-[10vh] ml-[40vw] mt-[5vh] flex flex-row">
+            <p class="font-afacad text-[5vw] font-bold text-[#000000]">Let's</p>
+            <p class="font-afacad text-[5vw] font-bold text-[#F9B401] ml-[1vw]">Start</p>
+        </div>
+
+
+        <div class="relative w-[80vw] h-[40vh] mt-[10vh] ml-[10vw] flex flex-row gap-[7vw]">
+            <img src="{{ asset('images/juicelab-start-one.svg') }}" alt="JuiceLab Start One">
+            <img src="{{ asset('images/juicelab-start-two.svg') }}" alt="JuiceLab Start Two">
+            <img src="{{ asset('images/juicelab-start-three.svg') }}" alt="JuiceLab Start Three">
+        </div>
+
+        <p class="relative text-black text-[5vw] ml-[30vw] mt-[10vh] font-afacad font-bold">Choose 3 kind of fruit</p>
+
+        <div class="relative w-[76vw] h-[60vh] mt-[10vh] ml-[12vw] flex flex-col gap-[5vh]">
+            <div class="relative flex flex-row gap-[3.1vw]">
+                <img src="{{ asset('images/juicelab-buah-1.svg') }}" alt="JuiceLab Buah 1">
+                <img src="{{ asset('images/juicelab-buah-2.svg') }}" alt="JuiceLab Buah 2">
+                <img src="{{ asset('images/juicelab-buah-3.svg') }}" alt="JuiceLab Buah 3">
+                <img src="{{ asset('images/juicelab-buah-4.svg') }}" alt="JuiceLab Buah 4">
+                <img src="{{ asset('images/juicelab-buah-5.svg') }}" alt="JuiceLab Buah 5">
+                <img src="{{ asset('images/juicelab-buah-6.svg') }}" alt="JuiceLab Buah 6">
+            </div>
+
+
+            <div class="relative flex flex-row gap-[3.1vw]">
+                <img src="{{ asset('images/juicelab-buah-7.svg') }}" alt="JuiceLab Buah 7">
+                <img src="{{ asset('images/juicelab-buah-8.svg') }}" alt="JuiceLab Buah 8">
+                <img src="{{ asset('images/juicelab-buah-9.svg') }}" alt="JuiceLab Buah 9">
+                <img src="{{ asset('images/juicelab-buah-10.svg') }}" alt="JuiceLab Buah 10">
+                <img src="{{ asset('images/juicelab-buah-11.svg') }}" alt="JuiceLab Buah 11">
+                <img src="{{ asset('images/juicelab-buah-12.svg') }}" alt="JuiceLab Buah 12">
+            </div>
+
+
+            <div class="relative flex flex-row text-center">
+                <div
+                    class="font-bold relative font-afacad text-white text-[2vw] rounded-[2vw] h-[5vh] w-[10vw] bg-[#FF7676]">
+                    Clear
+                </div>
+
+                <div
+                    class="font-bold ml-[60vw] relative font-afacad text-white text-[2vw] rounded-[2vw] h-[5vh] w-[10vw] bg-[#9DD071]">
+                    Done
+                </div>
+            </div>
+        </div>
+
+
+    </div>
 </x-layout>
